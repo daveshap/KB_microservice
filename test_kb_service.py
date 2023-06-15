e4ac0bb15376387fc15b5d8b4cd799366ada939d
David Shapiro <daveshap37@gmail.com>
2023-06-15 09:11:01 -0400

    done for the day, will test tomorrow

---
+++ test_kb_service.py
@@ -0,0 +1,53 @@
+import requests
+import json
+
+
+
+def test_create_endpoint():
+    text = input("Enter the text for the new KB article: ")
+    payload = {"input": text}
+    response = requests.post("http://localhost:999/create", json=payload)
+    print(response.json())
+
+
+
+def test_search_endpoint():
+    query = input("Enter the search query: ")
+    payload = {"query": query}
+    response = requests.post("http://localhost:999/search", json=payload)
+    print(response.json())
+
+
+
+def test_update_endpoint():
+    title = input("Enter the title of the KB article to update: ")
+    text = input("Enter the new text for the KB article: ")
+    payload = {"title": title, "input": text}
+    response = requests.post("http://localhost:999/update", json=payload)
+    print(response.json())
+
+
+
+def main():
+    while True:
+        print("\n\n\n1. Create KB article")
+        print("2. Search KB articles")
+        print("3. Update KB article")
+        print("4. Exit")
+        choice = input("Enter your choice: ")
+        if choice == '1':
+            test_create_endpoint()
+        elif choice == '2':
+            test_search_endpoint()
+        elif choice == '3':
+            test_update_endpoint()
+        elif choice == '4':
+            break
+        else:
+            print("Invalid choice. Please enter a number between 1 and 4.")
+
+
+
+
+if __name__ == "__main__":
+    main()
